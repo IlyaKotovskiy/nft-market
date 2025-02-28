15c7c9bf98b01f5fccc1b0543cceba83f1058d99
IlyaKotovskiy <ilya.kot.web@gmail.com>
2025-02-28 11:33:42 +0300

    🐛 fix: useClickOutside bug. A dropdown was always revealed when clicking anywhere. v2

--- packages/frontend/src/hooks/useClickOutside.ts
+++ packages/frontend/src/hooks/useClickOutside.ts
@@ -1,16 +1,22 @@
 import { useEffect, useCallback } from "react";
 
-export const useClickOutside = (ref: React.RefObject<HTMLElement>, callback: () => void) => {
+export const useClickOutside = (
+    ref: React.RefObject<HTMLElement>,
+    callback: () => void,
+    isActive: boolean
+) => {
     const memoizedCallback = useCallback(callback, []);
 
     useEffect(() => {
+        if (!isActive) return;
+
         const handleClickOutside = (event: MouseEvent) => {
             if (ref.current && !ref.current.contains(event.target as Node)) {
                 memoizedCallback();
             }
         };
-        document.addEventListener("mousedown", handleClickOutside);
 
+        document.addEventListener("mousedown", handleClickOutside);
         return () => document.removeEventListener("mousedown", handleClickOutside);
-    }, [memoizedCallback, ref]);
+    }, [memoizedCallback, ref, isActive]);
 };
