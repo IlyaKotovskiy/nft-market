cd9f1405258b45c82792571c5efda10a781160ce
IlyaKotovskiy <ilya.kot.web@gmail.com>
2025-05-14 19:51:35 +0300

    rolling back a previous commit

--- packages/frontend/src/api/index.ts
+++ packages/frontend/src/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: import.meta.env.MODE !== 'production' 
         ? 'http://localhost:3001/api' 
-        : `${import.meta.env.VITE_BACKEND}/api`,
+        : import.meta.env.VITE_BACKEND,
     headers: {
         'x-wallet-address': localStorage.getItem('wallet_address')
     }
