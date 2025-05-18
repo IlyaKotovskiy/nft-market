724a2e6d0153450a7a405a01d901bfd8b7d02e64
IlyaKotovskiy <ilya.kot.web@gmail.com>
2025-05-18 12:14:22 +0300

    fix import route middlewares

--- packages/backend/routes/NFT.routes.ts
+++ packages/backend/routes/NFT.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createNft, deleteNft, getNfts } from "../controllers/NFT.controller.js";
 import { upload } from "../config/multerConfig.js";
-import { requireAdmin } from "middlewares/adminMiddleware.js";
+import { requireAdmin } from "../middlewares/adminMiddleware.js";
 
 export const NFTRoutes = (router: Router) => {
     router.get('/nfts', getNfts);
