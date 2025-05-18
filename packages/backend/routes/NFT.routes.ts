2d31f3a313fac9c461625825d4deed4e1a20aed3
IlyaKotovskiy <ilya.kot.web@gmail.com>
2025-05-18 12:07:00 +0300

    secure routes on the backend

--- packages/backend/routes/NFT.routes.ts
+++ packages/backend/routes/NFT.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createNft, deleteNft, getNfts } from "../controllers/NFT.controller.js";
 import { upload } from "../config/multerConfig.js";
+import { requireAdmin } from "middlewares/adminMiddleware.js";
 
 export const NFTRoutes = (router: Router) => {
     router.get('/nfts', getNfts);
-    router.post('/create-nft', upload.single('file'), createNft);
-    router.delete('/delete-nft', deleteNft);
+    router.post('/create-nft', requireAdmin, upload.single('file'), createNft);
+    router.delete('/delete-nft', requireAdmin, deleteNft);
 }
